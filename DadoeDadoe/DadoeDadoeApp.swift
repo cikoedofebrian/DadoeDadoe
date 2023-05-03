c7ce1c9b0d823c38e7cb5b3e1a0b6d24b6828506
cikoedofebrian <ciko.edo.febrians@gmail.com>
2023-05-03 19:30:17 +0700

    Initial Commit

---
+++ DadoeDadoe/DadoeDadoeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DadoeDadoeApp.swift
+//  DadoeDadoe
+//
+//  Created by Ciko Edo Febrian on 03/05/23.
+//
+
+import SwiftUI
+
+@main
+struct DadoeDadoeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
